1b7d743dbe28791ffd5b006ea93f80a4b832323c
Saimon Ribeiro <saimonribeiros@hotmail.com>
2022-04-03 21:19:55 -0300

    init

---
+++ src/main/java/com/br/completedToDo/CompletedToDoApplication.java
@@ -0,0 +1,13 @@
+package com.br.completedToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletedToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletedToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/completedToDo/CompletedToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.completedToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletedToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
